6a4e0f6ca45fa57630aee78a8daa1519434c050e
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-12-12 19:32:21 -0300

    login as root

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     root 'home#logged', as: :authenticated_root
   end
 
-  root 'home#index'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
