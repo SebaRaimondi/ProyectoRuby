e196c55b157a48ee290b46f41e8d671fbca6af06
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-12-12 03:24:47 -0300

    Devise setup, Log in, log out, default logged route, sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  get 'home/logged'
+
+  authenticated :user do
+    root 'home#logged', as: :authenticated_root
+  end
+
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
