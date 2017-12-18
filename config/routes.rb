7407c1ede46eb89ad7f17adf7fae4eab28ea84f4
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-12-18 19:12:54 -0300

    tidy up routes, small changes views courses, exams

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   resources :courses do
-    resources :exams
-  end
-
-  resources :courses do
-    resources :students
+    resources :exams, except: :show
+    resources :students, except: :show
     post 'results', to: 'courses#results'
   end
 
