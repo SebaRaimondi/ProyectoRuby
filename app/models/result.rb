7b1f5fd113a299c5f982b0afea5e4292af50b3e0
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-12-18 21:10:31 -0300

    validation result

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,5 +2,7 @@ class Result < ApplicationRecord
   belongs_to :exam
   belongs_to :student
 
+  validates :mark, presence: true, numericality: { only_integer: true, moroe_than_or_equal_to: 0 }
+
   scope :for, ->(student, exam) { where(student: student, exam: exam) }
 end
