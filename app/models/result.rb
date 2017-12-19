6316b7282465e44f80eafa67dee7b8808ec0f12c
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-12-18 22:14:36 -0300

    Cursos aprobados, desaprobados, ausentes, porcentaje

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,4 +5,6 @@ class Result < ApplicationRecord
   validates :mark, presence: true, numericality: { only_integer: true, moroe_than_or_equal_to: 0 }
 
   scope :for, ->(student, exam) { where(student: student, exam: exam) }
+  scope :passed, ->(exam, min) { where('exam_id = ?', exam).where('mark >= ?', min) }
+  scope :failed, ->(exam, min) { where('exam_id = ?', exam).where('mark < ?', min) }
 end
