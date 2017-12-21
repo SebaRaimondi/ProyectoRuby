cd2b32341bb9e166c39cc42d0252af0720f802fb
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-12-21 17:21:13 -0300

    result tests

--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -4,4 +4,22 @@ class ResultTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'mark can only be integer or nil' do
+    res = results(:one)
+    res.mark = 'Soy una nota'
+    assert_not res.save
+  end
+
+  test 'mark cant be negative' do
+    res = results(:one)
+    res.mark = -2
+    assert_not res.save
+  end
+
+  test 'mark can be nil' do
+    res = results(:one)
+    res.mark = nil
+    assert res.save
+  end
 end
