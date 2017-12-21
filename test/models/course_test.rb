e644dea2fa0525aa27ebd78e561347e3a6f91ae4
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-12-21 16:06:36 -0300

    tests controllers removed, fix fixtures two repeated, test course model

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -46,8 +46,25 @@ class CourseTest < ActiveSupport::TestCase
 
   test 'should build full_title' do
     course = courses(:one)
-    course.title = 'soy un titulo'
+    course.title = 'Soy Un Titulo'
     course.year = 2017
-    assert course.full_title = 'Soy Un Titulo - 2017'
+    assert course.full_title == 'Soy Un Titulo - 2017'
+  end
+
+  # Associations
+  test 'should associate students' do
+    course = courses(:one)
+    assert course.students.size == 3
+    assert course.students.include? students(:one)
+    assert course.students.include? students(:two)
+    assert course.students.include? students(:three)
+  end
+
+  test 'should associate exams' do
+    course = courses(:one)
+    assert course.exams.size == 3
+    assert course.exams.include? exams(:one)
+    assert course.exams.include? exams(:two)
+    assert course.exams.include? exams(:three)
   end
 end
